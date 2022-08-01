076904dd21a7e65d2a635ff9a7a41db35464bd95
abantoo <abantonaimur@gmail.com>
2022-08-02 01:12:40 +0600

    added animation in footer buttons

--- src/components/Footer/footer.vue
+++ src/components/Footer/footer.vue
@@ -6,25 +6,20 @@
     <div class="p-2 border-b border-black">
       <div class="flex items-center justify-center">
         <a href="https://www.facebook.com/owheyitsme/">
-          <span>
-            <img src="../../assets/icons8-facebook-36.svg" alt="">
-          </span>
+            <img class="hover:animate-spin" src="../../assets/icons8-facebook-36.svg" alt="facebook-logo">
         </a>
         <a href="">
-          <span>
-            <img src="../../assets/icons8-instagram.svg" alt="">
-          </span>
+            <img class="hover:animate-bounce" src="../../assets/icons8-instagram.svg" alt="instagram-logo">
+
         </a>
         <a href="https://www.twitter.com/">
-          <span><img src="../../assets/icons8-twitter-circled.svg" alt=""></span>
+          <img class="hover:animate-spin w-full" src="../../assets/icons8-twitter-circled.svg" alt="twitter-logo">
         </a>
         <a href="">
-          <span><img src="../../assets/icons8-github.svg" alt=""></span>
+          <img class="hover:animate-spin" src="../../assets/icons8-github.svg" alt="github">
         </a>
         <a href="">
-          <span>
-            <img src="../../assets/icons8-linkedin.svg" alt="">
-          </span>
+            <img class="hover:animate-bounce" src="../../assets/icons8-linkedin.svg" alt="linkedin">
         </a>
       </div>
     </div>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import "./css/tailwind.css";
 import App from "./Views/App.vue";
+import 'flowbite';
 
 createApp(App).mount("#app");
