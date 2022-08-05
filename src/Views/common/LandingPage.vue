1fd7d14cc6bc58a6ff33ce6ecd76f87b461ab9ca
naimur-abanto <naimur@nerddevs.com>
2022-08-05 23:36:19 +0600

    added button stuffs

--- src/Views/common/LandingPage.vue
+++ src/Views/common/LandingPage.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="flex app-div">
-        
-        <div class="absolute top-[20vh] m-4 p-4">
-            <h1 class="text-9xl text-white font-bold">Welcome <br> 
-            To <br> Project <span class="inline-block animate-bounce stroke-blackw"> Yellow</span></h1>
-        </div>
+  <div class="flex app-div">
+    <div class="absolute top-[20vh] m-4 p-4">
+      <h1 class="text-9xl text-white font-bold">
+        Welcome <br />
+        To <br />
+        Project
+        <span class="inline-block animate-bounce">Poka</span>
+      </h1>
     </div>
+  </div>
 </template>
-
-
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}", "./node_modules/flowbite/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('flowbite/plugin')
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
   ],
+  theme: {
+    extend: {
+      animation: { "bounce-once": "bounce 1s", "ping-once": "ping 1s" },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
 };
