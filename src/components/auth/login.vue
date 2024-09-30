7592ffe6427f1442a9232a42fe372867b87bd3ba
abantoo <abantonaimur@gmail.com>
2024-09-30 23:41:53 +0600

    moves some components from views

---
+++ src/components/auth/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>
+      Already a poka?
+      <button type="button">
+        Login
+      </button>
+    </div>
+    <div>
+      Don't have an account? Create one!
+    </div> 
+  </div>
+</template>
---
+++ src/components/auth/signup.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>
+      Want to be a poka? Create an account now!
+      <button type="button">
+        Signup
+      </button>
+    </div>
+    <div>
+      Already a poka? Login!
+    </div> 
+  </div>
+</template>
